01bb8f3c8a2842218b8f2a0962d675fe80c5d62f
James Arnold <j1mesu@hotmail.co.uk>
2021-10-14 12:08:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// const mathFuncs = require("./maths")
+// const os = require("os");
+// const fs = require("fs");
+
+const _ = require("lodash");
+
+var array = [1, 2, 3];
+ 
+_.reverse(array);
+// => [3, 2, 1]
+ 
+console.log(array);
+// => [3, 2, 1]
+
+// let userDetails = os.userInfo().username;
+
+// fs.appendFile("oh-hi.txt", `Hello ${userDetails}`, (err) => {
+//     if (err){
+//         console.log("oops");
+//     }
+// })
+
+
+
+// console.log(add(2,5))
+// console.log(mathFuncs.multiply(2,5))
+
---
+++ maths.js
@@ -0,0 +1,9 @@
+    const multiply = (num1, num2) => {
+        return num1 * num2
+    }
+
+    module.exports = {
+        name: "maths.js",
+        desc: "this file contains a function to add two numbers together.",
+        multiply
+    }
